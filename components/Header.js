2ef764352c352e102bccce0578222a65decebb81
akshaysoin <144746642+akshaysoin@users.noreply.github.com>
2025-08-17 12:41:51 -0700

    Update Header.js
    Installed MOPO agent

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,13 @@
 export default function Header({ title }) {
   return (
     <>
+      <script
+        id="mopo_agent_embed_script"
+        type="text/javascript"
+        src="https://devagent.mopo.life/chat_assets/embed.js?url=https://mak.com"
+        crossorigin
+      ></script>
+
       <h1 className="title">{title}</h1>
     </>
   )
