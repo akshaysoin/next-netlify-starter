9965277189c418a0b5c0bdf22892f7e0e28e6feb
akshaysoin <144746642+akshaysoin@users.noreply.github.com>
2025-08-17 12:47:05 -0700

    Update Header.js
    Removed MOPO agent installation code

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,6 @@
 export default function Header({ title }) {
   return (
     <>
-      <script
-        id="mopo_agent_embed_script"
-        type="text/javascript"
-        src="https://devagent.mopo.life/chat_assets/embed.js?url=https://mak.com"
-        crossorigin
-      ></script>
-
       <h1 className="title">{title}</h1>
     </>
   )
