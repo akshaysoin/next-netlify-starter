211b9665e014a57aa1414c15bba5a0c997210f70
akshaysoin <144746642+akshaysoin@users.noreply.github.com>
2025-08-17 16:43:34 -0700

    Update test.js
    Fixed agent installation issue

--- pages/test.js
+++ pages/test.js
@@ -9,26 +9,28 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
 
-        <script
-          id="mopo_agent_embed_script"
-          type="text/javascript"
-          src="https://devagent.mopo.life/chat_assets/embed.js?url=https://mak.com"
-          crossorigin
-        >
+        <script type="text/javascript">
           // A unique global variable to act as a flag
           const MOPO_AGENT_LOADED_FLAG = 'window.MOPO_AgentLoaded';
-  
+      
           // Check if the flag is already set in the top-level window.
           // window.top refers to the highest window in the hierarchy.
-          if (window.top[MOPO_AGENT_LOADED_FLAG]) {
-            console.log("MOPO Agent already loaded. Preventing recursion.");
-          } else {
+          if (!window.top[MOPO_AGENT_LOADED_FLAG]) {
             // Set the flag to indicate the agent is now loading.
             window.top[MOPO_AGENT_LOADED_FLAG] = true;
             console.log("Initializing MOPO agent for the first time.");
+            
+            const script = document.createElement('script');
+            script.id = 'mopo_agent_embed_script';
+            script.type = 'text/javascript';
+            script.src = 'https://devagent.mopo.life/chat_assets/embed.js?url=https://mak.com';
+            script.crossOrigin = 'anonymous';
+            document.head.appendChild(script);
+          } else {
+            console.log("MOPO Agent already loaded. Preventing recursion.");
           }
-          </script>
-
+        </script>
+          
       </Head>
 
       <main>
