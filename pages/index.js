51e8ff40320f91d4185b50546f65d307b2d95441
akshaysoin <144746642+akshaysoin@users.noreply.github.com>
2025-08-17 19:24:00 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <script
           id="mopo_agent_embed_script"
           type="text/javascript"
-          src="https://devagent.mopo.life/chat_assets/embed.js?url=https://mak.com"
+          src="./embed.js?url=https://mak.com"
           crossorigin
         ></script>
             
