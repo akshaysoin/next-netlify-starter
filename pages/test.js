b5f75297a13bebbf750c1bb71dabc9bda6a34afc
akshaysoin <144746642+akshaysoin@users.noreply.github.com>
2025-08-17 19:25:20 -0700

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -9,34 +9,12 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
 
-        <script 
-          dangerouslySetInnerHTML={{
-            __html: ` //There is a quote on this line!
-              (function() {
-                // A unique global variable to act as a flag
-                const MOPO_AGENT_LOADED_FLAG = 'MOPO_AgentLoaded';
-        
-                // Check if the flag is already set in the top-level window.
-                // window.top refers to the highest window in the hierarchy.
-                if (!window.top[MOPO_AGENT_LOADED_FLAG]) {
-                  // Set the flag to indicate the agent is now loading.
-                  window.top[MOPO_AGENT_LOADED_FLAG] = true;
-                  console.log("Initializing MOPO agent for the first time.");
-                  
-                  const script = document.createElement('script');
-                  script.id = 'mopo_agent_embed_script';
-                  script.type = 'text/javascript';
-                  script.src = 'https://devagent.mopo.life/chat_assets/embed.js?url=https://mak.com';
-                  script.crossOrigin = 'anonymous';
-                  document.head.appendChild(script);
-                } else {
-                  //Skip loading agent as it is already loaded
-                  console.log("MOPO Agent already loaded. Preventing recursion.");
-                }
-              })();
-            ` //There is a quote on this line!
-          }}
-        />
+        <script
+          id="mopo_agent_embed_script"
+          type="text/javascript"
+          src="./embed.js?url=https://mak.com"
+          crossorigin
+        ></script>
           
       </Head>
 
